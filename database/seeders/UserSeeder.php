faa09d760358286897773abb8894d4581e3227d8
NguyenTungBach <bachntth2010055@fpt.edu.vn>
2023-06-12 15:08:24 +0700

    Bach update seed db

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-      DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+//      DB::statement('SET FOREIGN_KEY_CHECKS = 0');
       DB::table('users')->truncate();
       DB::table('users')->insert([
         [
@@ -54,6 +54,6 @@ class UserSeeder extends Seeder
           'status' => 1,
         ],
       ]);
-      DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+//      DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
