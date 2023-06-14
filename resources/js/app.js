998d61ff77b28108aa43462caf7d4028a7c18f18
HaiYen0110 <haiyentp.1204@gmail.com>
2023-06-15 01:26:41 +0700

    mockup working list

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,8 @@ import './permission';
 import VueAnime from 'vue-animejs';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import VueApexCharts from 'vue-apexcharts';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 // import './style/index.scss';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -15,6 +17,7 @@ Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.use(VueAnime);
 Vue.use(VueApexCharts);
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 Vue.component('Apexchart', VueApexCharts);
 new Vue({
