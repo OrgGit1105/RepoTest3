0ceb5e76d0e90f0e16baf248105d63b2c9089224
maitue <maivantue29@gmail.com>
2023-07-19 11:12:47 +0700

    [BE][DEV] fix show emotion

--- app/Http/Controllers/Api/AnalyticController.php
+++ app/Http/Controllers/Api/AnalyticController.php
@@ -77,6 +77,142 @@ class AnalyticController extends Controller
 
     /**
      * @OA\Get(
+     *   path="/api/analytic/{id}",
+     *   tags={"Analytic"},
+     *   summary="Detail Analytic",
+     *   operationId="analytic_show",
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     required=true,
+     *     @OA\Schema(
+     *      type="string",
+     *     ),
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Send request success",
+     *     @OA\MediaType(
+     *      mediaType="application/json",
+     *      example={"code":200,"data":{"id": 1,"name":"......"}}
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=401,
+     *     description="Login false",
+     *     @OA\MediaType(
+     *      mediaType="application/json",
+     *      example={"code":401,"message":"Username or password invalid"}
+     *     )
+     *   ),
+     *   security={{"auth": {}}},
+     * )
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        try {
+            $department = $this->repository->find($id);
+            return $this->responseJson(200, new BaseResource($department));
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * @OA\Post(
+     *   path="/api/analytic/{id}",
+     *   tags={"Analytic"},
+     *   summary="Update Analytic",
+     *   operationId="analytic_update",
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     required=true,
+     *     @OA\Schema(
+     *      type="string",
+     *     ),
+     *   ),
+     *   @OA\RequestBody(
+     *       @OA\MediaType(
+     *          mediaType="application/json",
+     *          example={"name":"string"},
+     *          @OA\Schema(
+     *            required={"name"},
+     *            @OA\Property(
+     *              property="name",
+     *              format="string",
+     *            ),
+     *         )
+     *      )
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Send request success",
+     *     @OA\MediaType(
+     *      mediaType="application/json",
+     *      example={"code":200,"data":{"id": 1,"name":  "............."}}
+     *     ),
+     *   ),
+     *   @OA\Response(
+     *     response=403,
+     *     description="Access Deny permission",
+     *     @OA\MediaType(
+     *      mediaType="application/json",
+     *      example={"code":403,"message":"Access Deny permission"}
+     *     ),
+     *   ),
+     *   security={{"auth": {}}},
+     * )
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(AnalyticRequest $request, $id)
+    {
+        $attributes = $request->except([]);
+        $data = $this->repository->update($attributes, $id);
+        return $this->responseJson(200, new BaseResource($data));
+    }
+
+    /**
+     * @OA\Delete(
+     *   path="/api/analytic/{id}",
+     *   tags={"Analytic"},
+     *   summary="Delete ..............",
+     *   operationId="analytic_delete",
+     *   @OA\Parameter(
+     *      name="id",
+     *      in="path",
+     *      required=true,
+     *     @OA\Schema(
+     *      type="string",
+     *     ),
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Send request success",
+     *     @OA\MediaType(
+     *      mediaType="application/json",
+     *      example={"code":200,"data":"Send request success"}
+     *     )
+     *   ),
+     *   security={{"auth": {}}},
+     * )
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    public function destroy($id)
+    {
+        $this->repository->delete($id);
+        return $this->responseJson(200, null, trans('messages.mes.delete_success'));
+    }
+
+    /**
+     * @OA\Get(
      *   path="/api/analytic/emotions",
      *   tags={"Analytic"},
      *   summary="List emotion",
