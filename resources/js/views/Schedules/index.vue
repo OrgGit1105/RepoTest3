fe7d4288195b8289d10fa13e3b47b062711282ff
maitue <maivantue29@gmail.com>
2023-07-20 18:12:52 +0700

    [FE] show modal schedules

--- resources/js/views/Schedules/index.vue
+++ resources/js/views/Schedules/index.vue
@@ -1,48 +1,127 @@
 <template>
-  <full-calendar 
-   :events="fcEvents" 
-   :locale="'ja'" 
-   @changeMonth="changeMonth"
-    >
-  </full-calendar>
+  <div>
+    <div>
+      <full-calendar 
+        :events="fcEvents" 
+        locale="en"
+        @changeMonth="changeMonth"
+        @eventClick="eventClick"
+        @dayClick="dayClick">
+      </full-calendar>
+    </div>
+    <b-modal v-model="modalShow" :title="this.date_click" centered id="modal-center">
+      <div v-for="item in this.one_day">
+        <div :class="item.title.includes('Remote') ? 'remote' : 'take-off'" >{{item.title}}</div>
+      </div>
+      <template #modal-footer>
+        <div class="w-100">
+          <b-button
+            id="close-schedule"
+            variant="outline-secondary"
+            size="lg"
+            class="float-left"
+            @click="modalShow = false">
+            Close
+          </b-button>
+        </div>
+      </template>
+    </b-modal>
+  </div>
 </template>
 <script>
 import { getAllSchedules } from '../../api/schedules';
-// var demoEvents = this.dataSchedules;
 export default {
+    name: 'SchedulesManagement',
     data() {
-        return {
-            // dataSchedules: [],
-            fcEvents: [],
-            year_months: new Date().toISOString().substr(0, 7)
-        }
+      return {
+        fcEvents: [],
+        year_months: new Date().toISOString().substr(0, 7),
+        modalShow: false,
+        date_click: '',
+        one_day: []
+      }
     },
     components: {
-        'full-calendar': require('vue-fullcalendar')
+      'full-calendar': require('vue-fullcalendar')
     },
     created() {
-        this.getAllSchedulesByDate();
+      this.getAllSchedulesByDate();
     },
     methods: {
         async getAllSchedulesByDate() {
-            let PARAMS = {
-                year_month: this.year_months
+          let PARAMS = {
+            year_month: this.year_months
+          }
+          await getAllSchedules(PARAMS)
+          .then((response) => {
+            if (response.code === 200) {
+                this.fcEvents = response.data;
             }
-            await getAllSchedules(PARAMS)
-            .then((response) => {
-                if (response.code === 200) {
-                    this.fcEvents = response.data;
-                }
-            }).catch((error) => {
-                    this.fcEvents = [];
-            });
+          }).catch((error) => {
+            this.fcEvents = [];
+          });
         }, 
         'changeMonth'(start, end, current) {
-            this.year_months = new Date(current).toISOString().substr(0, 7);
-            this.getAllSchedulesByDate();
+          this.year_months = new Date(current).toISOString().substr(0, 7);
+          this.getAllSchedulesByDate();
         },
-        
+        'dayClick'(day, jsEvent) {
+          const date = new Date(day);
+          const year = date.getFullYear();
+          const month = String(date.getMonth() + 1).padStart(2, '0');
+          const days = String(date.getDate()).padStart(2, '0');
+          const dayClick = `${year}-${month}-${days}`;
+          const formattedDate = `${year}-${month}`;
+            if(this.year_months == formattedDate) {
+              this.modalShow = true;
+              const filterDate = new Date(dayClick);
+              const filteredArray = this.fcEvents.filter((obj) => {
+                  const objDate = new Date(obj.start);
+                  return objDate.getTime() === filterDate.getTime();
+              });
+              this.date_click = dayClick;
+              this.one_day = filteredArray;
+            }
+        },
+        'eventClick'(event, jsEvent, pos) {
+          this.month_click = new Date(event.start).toISOString().slice(0, 7);
+            if (this.year_months == this.month_click) { 
+              this.modalShow = true;
+              const filterDate = new Date(event.start);
+              const filteredArray = this.fcEvents.filter((obj) => {
+                  const objDate = new Date(obj.start);
+                  return objDate.getTime() === filterDate.getTime();
+              });
+              this.date_click = event.start;
+              this.one_day = filteredArray;
+            }
+        }
     }
-
 }
 </script>
+
+<style scoped>
+#close-schedule {
+  color: #70afe1;
+  padding: 0 40px 0 40px;
+  border-color:#70afe1 !important;
+}
+
+#close-schedule:hover {
+ background-color: white;
+}
+.remote {
+  background-color: #C7E6FD;
+  margin-bottom: 10px;
+  padding-left: 10px;
+  width: 50%;
+  border-radius: 10px;
+}
+.take-off {
+  background-color: antiquewhite;
+  margin-bottom: 10px;
+  padding-left: 10px;
+  width: 50%;
+  border-radius: 10px;
+}
+</style>
