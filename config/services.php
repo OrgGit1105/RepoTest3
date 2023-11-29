b0136832341b7ecf0c868c7c836ff0d450e4bf4a
VuHoa <vuhoa11052000@gmail.com>
2023-11-29 15:20:44 +0700

    update IAM role

--- config/services.php
+++ config/services.php
@@ -32,8 +32,8 @@ return [
 
     'aws' => [
       'urlImage' => 'https://v-face.s3.amazonaws.com/',
-      'AWS_DEFAULT_REGION' => 'ap-northeast-1',
-      'AWS_ACCESS_KEY_ID' => 'AKIA6IVJOOLI5DZ3IXNC',
-      'AWS_SECRET_ACCESS_KEY' => '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',
+      'AWS_DEFAULT_REGION' => env('AWS_DEFAULT_REGION'),
+      'AWS_ACCESS_KEY_ID' => env('AWS_ACCESS_KEY_ID'),
+      'AWS_SECRET_ACCESS_KEY' => env('AWS_SECRET_ACCESS_KEY'),
     ]
 ];
