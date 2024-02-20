d858bf2cbdf2b3a238fcb60a1a7364750147a92e
VuHoa <vuhoa11052000@gmail.com>
2024-02-20 16:23:18 +0700

    update

--- app/Repositories/PolicyRepository.php
+++ app/Repositories/PolicyRepository.php
@@ -472,7 +472,7 @@ class PolicyRepository extends BaseRepository implements PolicyRepositoryInterfa
             $commands[] = "sudo groupmod --new-name $groupNew $groupOld";
         } else {
             if ($projectOld) {
-                $command[] = "sudo chown -R :root /var/www/$projectOld";
+                $command[] = "sudo chown -R :apache /var/www/$projectOld";
                 $command[] = "sudo chmod -R 777 /var/www/$projectOld/storage/";
                 $command[] = "sudo chmod -R 775 /var/www/$projectOld/.git/";
                 $command[] = "sudo chmod g+s /var/www/$projectOld";
