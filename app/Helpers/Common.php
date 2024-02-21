8e86e7e0f31e80a9f5dfce57a98e9bfc9f9aeec5
VuHoa <vuhoa11052000@gmail.com>
2024-02-21 14:59:09 +0700

    update

--- app/Helpers/Common.php
+++ app/Helpers/Common.php
@@ -186,8 +186,8 @@ class Common
                 'commands' => [
                     "if ! grep -q \"^$groupName:\" /etc/group; then sudo groupadd $groupName; fi",
                     "sudo chown -R :$groupName /var/www/$projectName", // thư mục thuộc về group, thuộc sở hữu của người dùng root
-                    "sudo chmod -R 777 /var/www/$projectName/storage/",
-                    "sudo chmod -R 775 /var/www/$projectName/.git/",
+                    "find /var/www/$projectName -type d -name \"storage\" -exec chmod -R 777 {} \;",
+                    "find /var/www/$projectName -type d -name \".git\" -exec chmod -R 775 {} \;",
                     "sudo chmod g+s /var/www/$projectName" // đảm bảo rằng tất cả các thư mục con được tạo trong đó sẽ kế thừa nhóm của thư mục gốc
                 ],
             ],
