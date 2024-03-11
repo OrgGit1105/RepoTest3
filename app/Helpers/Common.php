c44f1384c807faaa175451caa478e81eba0787b6
VuHoa <vuhoa11052000@gmail.com>
2024-03-11 17:18:35 +0700

    no message

--- app/Helpers/Common.php
+++ app/Helpers/Common.php
@@ -233,7 +233,7 @@ class Common
                     "find /var/www/$projectName -type d -name \"storage\" -exec chmod -R 777 {} \;",
                     "find /var/www/$projectName -type d -name \".git\" -exec chmod -R 777 {} \;",
                     "find /var/www/$projectName -type d -path \"*/public/js\" -exec chmod -R 775 {} \;",
-                    "sudo chmod g+s /var/www/$projectName", // đảm bảo rằng tất cả các thư mục con được tạo trong đó sẽ kế thừa nhóm của thư mục gốc
+                    "sudo chmod -R g+s /var/www/$projectName", // đảm bảo rằng tất cả các thư mục con được tạo trong đó sẽ kế thừa nhóm của thư mục gốc
                     "if id -u apache > /dev/null 2>&1; then sudo usermod -aG $groupName apache; fi", // thêm tk apache vào nhóm
                     "for user in \$(getent group $groupOldOfProject | cut -d: -f4 | tr ',' ' '); do sudo usermod -aG $groupName \$user; done", // thêm tk ec2-user, apache, deploy vào nhóm
                 ],
