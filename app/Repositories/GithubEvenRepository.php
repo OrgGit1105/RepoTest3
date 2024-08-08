bfee17247e54bae3fa952675a9419c7a43c991fd
cuong ngo tuan <tuancuongth88@gmail.com>
2024-08-08 17:17:58 +0700

    tesst

--- app/Repositories/GithubEvenRepository.php
+++ app/Repositories/GithubEvenRepository.php
@@ -32,29 +32,35 @@ class GithubEvenRepository extends BaseRepository implements GithubEvenRepositor
     }
 
     public function createIssues(Request $request){
-        $data = $request->getContent();
+        try {
+            Log::info('Cloudwatch Alarm:', $request->all());
+            $data = $request->getContent();
 
-        // Ghi log dữ liệu nhận được để kiểm tra
-        Log::info('Received SNS Notification:', ['data' => $data]);
+            // Ghi log dữ liệu nhận được để kiểm tra
+            Log::info('Received SNS Notification:', ['data' => $data]);
 
-        // Giả sử dữ liệu nhận được là JSON và chuyển đổi nó thành mảng
-        $dataArray = json_decode($data, true);
+            // Giả sử dữ liệu nhận được là JSON và chuyển đổi nó thành mảng
+            $dataArray = json_decode($data, true);
 
-        // Kiểm tra nếu đây là yêu cầu xác thực
-        if (isset($dataArray['Type']) && $dataArray['Type'] === 'SubscriptionConfirmation') {
-            $subscribeUrl = $dataArray['SubscribeURL'];
+            // Kiểm tra nếu đây là yêu cầu xác thực
+            if (isset($dataArray['Type']) && $dataArray['Type'] === 'SubscriptionConfirmation') {
+                $subscribeUrl = $dataArray['SubscribeURL'];
 
-            // Thực hiện yêu cầu GET tới SubscribeURL để xác nhận
-            $response = Http::get($subscribeUrl);
+                // Thực hiện yêu cầu GET tới SubscribeURL để xác nhận
+                $response = Http::get($subscribeUrl);
 
-            // Ghi log kết quả xác nhận
-            Log::info('Subscription confirmation response:', ['status' => $response->status(), 'body' => $response->body()]);
+                // Ghi log kết quả xác nhận
+                Log::info('Subscription confirmation response:', ['status' => $response->status(), 'body' => $response->body()]);
 
-            return response()->json(['message' => 'Subscription confirmed'], 200);
+                return response()->json(['message' => 'Subscription confirmed'], 200);
+            }
+
+            // Xử lý các loại thông báo khác ở đây
+
+            return response()->json(['message' => 'Notification received'], 200);
+        }catch (\Exception $exception){
+            Log::error('Cloudwatch error',  $exception);
         }
 
-        // Xử lý các loại thông báo khác ở đây
-
-        return response()->json(['message' => 'Notification received'], 200);
     }
 }
