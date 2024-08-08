e12a7561d7e451b512a5ee42e886b6efea14e4b1
cuong ngo tuan <tuancuongth88@gmail.com>
2024-08-08 17:11:43 +0700

    fix

--- app/Repositories/GithubEvenRepository.php
+++ app/Repositories/GithubEvenRepository.php
@@ -32,15 +32,17 @@ class GithubEvenRepository extends BaseRepository implements GithubEvenRepositor
     }
 
     public function createIssues(Request $request){
-        // Lấy dữ liệu từ SNS
-        $data = $request->all();
+        $data = $request->getContent();
 
         // Ghi log dữ liệu nhận được để kiểm tra
-        Log::info('Received SNS Notification:', $data);
+        Log::info('Received SNS Notification:', ['data' => $data]);
+
+        // Giả sử dữ liệu nhận được là JSON và chuyển đổi nó thành mảng
+        $dataArray = json_decode($data, true);
 
         // Kiểm tra nếu đây là yêu cầu xác thực
-        if (isset($data['Type']) && $data['Type'] === 'SubscriptionConfirmation') {
-            $subscribeUrl = $data['SubscribeURL'];
+        if (isset($dataArray['Type']) && $dataArray['Type'] === 'SubscriptionConfirmation') {
+            $subscribeUrl = $dataArray['SubscribeURL'];
 
             // Thực hiện yêu cầu GET tới SubscribeURL để xác nhận
             $response = Http::get($subscribeUrl);
@@ -54,38 +56,5 @@ class GithubEvenRepository extends BaseRepository implements GithubEvenRepositor
         // Xử lý các loại thông báo khác ở đây
 
         return response()->json(['message' => 'Notification received'], 200);
-//        $alarmName = $request->input('alarmName');
-//        $alarmDescription = $request->input('description', 'No description provided.');
-//
-//        // Thay đổi URL và Token với thông tin GitHub của bạn
-//        $githubRepo = 'VEHO-Develop';
-//        $githubToken = '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';
-//        $githubUrl = "https://api.github.com/repos/TeckVeho/{$githubRepo}/issues";
-//
-//        $headers = [
-//            'Authorization' => "token {$githubToken}",
-//            'Accept' => 'application/vnd.github.v3+json'
-//        ];
-//
-//        $issueData = [
-//            'title' => "CloudWatch Alarm Triggered: {$alarmName}",
-//            'body' => "Alarm Description: {$alarmDescription}",
-//            'assignees' => ['tuancuongth88'], // Chuyển thành mảng
-//            'milestone' => 1, // GitHub API cần ID milestone, không phải tên
-//            'labels' => ['low priority'] // Chuyển thành mảng
-//        ];
-//
-//        $response = Http::withHeaders($headers)->post($githubUrl, $issueData);
-//
-//        if ($response->successful()) {
-//            return ResponseService::responseJson(Response::HTTP_OK, 'add issue success');
-//        } else {
-//            return response()->json([
-//                'statusCode' => $response->status(),
-//                'body' => $response->body()
-//            ], $response->status());
-//        }
-        return response()->json(['message' => 'Notification received'], 200);
-//        return ResponseService::responseJson(Response::HTTP_OK, 'add issue success');
     }
 }
