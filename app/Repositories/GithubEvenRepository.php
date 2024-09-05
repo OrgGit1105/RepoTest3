148f443132f8d4fc8331edcbf8b776b9611ebb90
cuong ngo tuan <tuancuongth88@gmail.com>
2024-09-05 13:14:07 +0700

    add get info top server

--- app/Repositories/GithubEvenRepository.php
+++ app/Repositories/GithubEvenRepository.php
@@ -8,6 +8,9 @@
 namespace Repository;
 
 use App\Repositories\Contracts\GithubEvenRepositoryInterface;
+use Aws\AwsClient;
+use Aws\Ssm\SsmClient;
+use Helper\Common;
 use Helper\ResponseService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -32,6 +35,7 @@ class GithubEvenRepository extends BaseRepository implements GithubEvenRepositor
     }
 
     public function createIssues(Request $request){
+
         $data = $request->getContent();
 
         // Ghi log dữ liệu nhận được để kiểm tra
@@ -53,7 +57,6 @@ class GithubEvenRepository extends BaseRepository implements GithubEvenRepositor
 
             return response()->json(['message' => 'Subscription confirmed'], 200);
         }
-
         // Kiểm tra nếu đây là thông báo loại Notification
         if (isset($dataArray['Type']) && $dataArray['Type'] === 'Notification') {
             Log::info('Cloudwatch Alarm:', $dataArray);
@@ -80,49 +83,57 @@ class GithubEvenRepository extends BaseRepository implements GithubEvenRepositor
             $datapointsToAlarm = $decodedJson['Trigger']['DatapointsToAlarm'];
             $instance = $decodedJson['Trigger']['Dimensions'][0]['value'];
             $region = $decodedJson['Region'];
+            $contentTop = null;
+            if(explode('-', $dimensions[0]['value'])[0] == 'i' && count(explode('-', $dimensions[0]['value'])) > 1){
+                $responseTopServer = $this->getInfoTopServer($dimensions[0]['value']);
+                $contentTop = $responseTopServer['StandardOutputContent'];
+            }
             // Chuẩn bị nội dung chi tiết của issue
             $issueBody = "
-                You are receiving this email because your Amazon CloudWatch Alarm \"{$alarmName}\" in the {$region} region has entered the ALARM state, because \"{$stateChangeReason}\" at \"{$timestamp}\".
+You are receiving this email because your Amazon CloudWatch Alarm \"{$alarmName}\" in the {$region} region has entered the ALARM state, because \"{$stateChangeReason}\" at \"{$timestamp}\".
 
-                View this alarm in the AWS Management Console:
-                https://ap-northeast-1.console.aws.amazon.com/cloudwatch/deeplink.js?region=ap-northeast-1#alarmsV2:alarm/{$alarmName}
-
-
-                Alarm Details:
-                - Name:                       {$alarmName}
-                - Description:                {$alarmDescription}
-                - State Change:               OK -> ALARM
-                - Reason for State Change:    {$stateChangeReason}
-                - Timestamp:                  {$timestamp}
-                - AWS Account:                {$awsAccountId}
-                - Alarm Arn:                  {$alarmArn}
-
-                Threshold:
-                - The alarm is in the ALARM state when the metric is {$comparisonOperator} {$threshold} for at least {$datapointsToAlarm} of the last {$datapointsToAlarm} period(s) of {$period} seconds.
-
-                Monitored Metric:
-                - MetricNamespace:                     {$namespace}
-                - MetricName:                          {$metricName}
-                - Dimensions:                          " . $this->formatDimensions($dimensions) . "
-                - Period:                              {$period} seconds
-                - Statistic:                           {$statistic}
-                - Unit:                                not specified
-                - TreatMissingData:                    {$treatMissingData}
+View this alarm in the AWS Management Console:
+https://ap-northeast-1.console.aws.amazon.com/cloudwatch/deeplink.js?region=ap-northeast-1#alarmsV2:alarm/{$alarmName}
 
 
-                State Change Actions:
-                - OK:
-                - ALARM: [{$topicArn}]
-                - INSUFFICIENT_DATA:
+Alarm Details:
+- Name:                       {$alarmName}
+- Description:                {$alarmDescription}
+- State Change:               OK -> ALARM
+- Reason for State Change:    {$stateChangeReason}
+- Timestamp:                  {$timestamp}
+- AWS Account:                {$awsAccountId}
+- Alarm Arn:                  {$alarmArn}
+
+Threshold:
+- The alarm is in the ALARM state when the metric is {$comparisonOperator} {$threshold} for at least {$datapointsToAlarm} of the last {$datapointsToAlarm} period(s) of {$period} seconds.
+
+Monitored Metric:
+- MetricNamespace:                     {$namespace}
+- MetricName:                          {$metricName}
+- Dimensions:                          " . $this->formatDimensions($dimensions) . "
+- Period:                              {$period} seconds
+- Statistic:                           {$statistic}
+- Unit:                                not specified
+- TreatMissingData:                    {$treatMissingData}
 
 
-                --
-                If you wish to stop receiving notifications from this topic, please click or visit the link below to unsubscribe:
-                {$unsubscribeURL}&Endpoint=tuancuongth88@gmail.com
+State Change Actions:
+- OK:
+- ALARM: [{$topicArn}]
+- INSUFFICIENT_DATA:
 
-                Please do not reply directly to this email. If you have any questions or comments regarding this email, please contact us at <a href=\"https://aws.amazon.com/support\"> https://aws.amazon.com/support</a>
+
+--
+
+Infomation CPU Ram(Command top server)
+{$contentTop}
+
+If you wish to stop receiving notifications from this topic, please click or visit the link below to unsubscribe:
+{$unsubscribeURL}&Endpoint=tuancuongth88@gmail.com
+
+Please do not reply directly to this email. If you have any questions or comments regarding this email, please contact us at <a href=\"https://aws.amazon.com/support\"> https://aws.amazon.com/support</a>
             ";
-
             // Thay đổi URL và Token với thông tin GitHub của bạn
             $githubToken = config('common.github.github_token');
             $githubUrl = config('common.github.veho_develop.api_issues');
@@ -166,6 +177,39 @@ class GithubEvenRepository extends BaseRepository implements GithubEvenRepositor
         // Xử lý các loại thông báo khác ở đây (nếu có)
         return response()->json(['message' => 'Notification received'], 200);
 
+    }
+
+    private function getInfoTopServer($instanceId)
+    {
+        $param = ($instanceId != INSTANCE_ID_240) ? Common::configAwsSDK($instanceId) : Common::configAwsSDK();
+        $ssmClient = new SsmClient($param);
+
+        $parameters = [
+            'InstanceIds' => [$instanceId],
+            'DocumentName' => 'AWS-RunShellScript',
+            'Parameters' => [
+                'commands' => ["export COLUMNS=500; top -c -b -o +%CPU | head -n 20"],
+            ],
+        ];
+        $response = $ssmClient->sendCommand($parameters);
+        $commandId = $response['Command']['CommandId'];
+
+        $waitTime = 5;
+        $maxAttempts = 10;
+
+        for ($attempts = 0; $attempts < $maxAttempts; $attempts++) {
+            sleep($waitTime);
+            $output = $ssmClient->getCommandInvocation([
+                'CommandId' => $commandId,
+                'InstanceId' => $instanceId,
+            ]);
+
+            if ($output['Status'] === 'Success') {
+                return $output;
+            }
+        }
+
+        return $output; // Return the last output even if not successful
     }
 
     private function formatDimensions($dimensions)
